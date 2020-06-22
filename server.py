3e915a3845c213c82980a73887e4b278b26890e2
LaupWing <loc_strict@hotmail.com>
2020-06-22 12:24:01 +0200

    ep2

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
 import socket
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
 
+HEADERSIZE = 10
+
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcom to the server!", "utf-8"))
+
+    msg = "Welcom to the server!"
+    msg = f'{len(msg):<{HEADERSIZE}}'+msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
+
+    while True:
+        time.sleep(3)
+        msg =  f"The time is! {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}'+msg
+        clientsocket.send(bytes(msg, "utf-8"))
